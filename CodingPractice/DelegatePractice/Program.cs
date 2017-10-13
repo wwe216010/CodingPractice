a9f2bdd2aff90cf468ea9a8eca6ef52bb4aa7df5
wwe216010 <wwe951110101@gmail.com>
2017-10-13 15:51:30 +0800

    new add

---
+++ CodingPractice/DelegatePractice/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatePractice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassA A = new ClassA();
+            ClassB B = new ClassB();
+        }
+    }
+
+    public class ClassA
+    {
+        //public ClassA()
+        //{
+        //    ClassB B = new ClassB();
+        //    B.MethodB();
+        //}
+
+        //public void MethodA()
+        //{
+        //    Console.WriteLine("A被呼叫了~~");
+        //    Console.ReadLine();
+        //}
+
+
+        public ClassA()
+        {
+            ClassB.MyDelegate Del = new ClassB.MyDelegate(MethodA);
+            Del("Hello World~~");
+        }
+
+        public void MethodA(string i)
+        {
+            Console.WriteLine(i);
+            Console.ReadLine();
+        }
+    }
+
+    public class ClassB
+    {
+        //public ClassB()
+        //{
+        //    ClassA A = new ClassA();
+        //    A.MethodA();
+        //}
+
+        //public void MethodB()
+        //{
+        //    Console.WriteLine("B被呼叫了~~");
+        //    Console.ReadLine();
+        //}
+
+
+        public delegate void MyDelegate(string Para1);
+        public MyDelegate Del;
+
+        public ClassB()
+        {
+            Del = new MyDelegate(MethodB);
+            Del("World Hello~~");
+        }
+
+        public void MethodB(string i)
+        {
+            Console.WriteLine(i);
+            Console.ReadLine();
+        }
+    }
+}
--- CodingPractice/DelegateTest/Program.cs
+++ CodingPractice/DelegateTest/Program.cs
@@ -12,19 +12,25 @@ namespace DelegateTest
 
         static void Main(string[] args)
         {
-            Data d1 = new Data { Name = "Hao", Age = 30 };
+            Data data = new Data { Name = "Hao", Age = 30 };
 
 
-            Program P = new Program();
-            Del d = P.test;
-            d(d1);
+            //Program P = new Program();
+            //Del d = P.test;
+            //d(data);
+            //或下面寫法
+            Del d = new Del(test);
+            d(data);
         }
 
-        public void test(object o)
+        private static void test(object o)
         {
-            var qq = o as Data;//向下轉型，qq才能使用Data的成員。Data:qq
-            Console.WriteLine(qq.Name + qq.Age);
-            Console.ReadLine();
+            if (o is Data)//檢查o是否為Data類別向上轉型
+            {
+                var qq = o as Data;//向下轉型，qq才能使用Data的成員。Data繼承qq
+                Console.WriteLine(qq.Name + qq.Age);
+                Console.ReadLine();   
+            }
         }
     }
 
